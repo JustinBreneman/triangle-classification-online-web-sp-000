92330017111b8aa0bda4a2ed8f8b56f9968f8e39
Justin Breneman <justin.breneman@yahoo.com>
2020-06-20 03:44:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,12 +6,17 @@ class Triangle
     @side_c = side_c
   end
   def kind
-    if side_a == side_b && side_b == side_c
-      :equilateral
-    elsif side_a == side_b || side_b == side_c || side_a == side_c
-      :isosceles
-    elsif side_a != side_b && side_b != side_c && side_a != side_c
-      :scalene
+    if @side_a == 0 || @side_b == 0 || @side_c == 0
+      raise TriangleError
+      if @side_a == @side_b && @side_b == @side_c
+        :equilateral
+      elsif @side_a == @side_b || @side_b == @side_c || @side_a == @side_c
+        :isosceles
+      elsif @side_a != @side_b && @side_b != @side_c && @side_a != @side_c
+        :scalene
+      end
     end
+  end
+  class TriangelError < StandardError
   end
 end
