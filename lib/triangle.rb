007bc82071871fe000bbdfdee7294efef06b93a4
Justin Breneman <justin.breneman@yahoo.com>
2020-06-20 03:45:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,6 +8,7 @@ class Triangle
   def kind
     if @side_a == 0 || @side_b == 0 || @side_c == 0
       raise TriangleError
+    else
       if @side_a == @side_b && @side_b == @side_c
         :equilateral
       elsif @side_a == @side_b || @side_b == @side_c || @side_a == @side_c
