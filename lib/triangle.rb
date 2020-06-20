295c6206ac0bef1a3d9387cfff2e2c8f17f673cd
Justin Breneman <justin.breneman@yahoo.com>
2020-06-20 03:47:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,7 @@ class Triangle
     @side_c = side_c
   end
   def kind
-    if @side_a == 0 || @side_b == 0 || @side_c == 0
+    if @side_a <= 0 || @side_b <= 0 || @side_c <= 0
       raise TriangleError
     else
       if @side_a == @side_b && @side_b == @side_c
